7df5a7b9dc3b2bfedaacd7e3ff0b3a3025cfe4d2
Dmitriy <example@mail.com>
2020-11-07 14:32:05 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+   return <header className="header">
+      <img src="https://placeit-assets1.s3-accelerate.amazonaws.com/custom-pages/technology-logo-maker-lp/online-logo-design-template-for-an-eco-tech-company-2176l-206-el-1024x1024.png" alt="" />
+   </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+   return <div className="content">
+      <div>
+         <img src="https://theinpaint.com/images/example-1-2.jpg" alt="" />
+      </div>
+      <div>
+         ava + descr
+   </div>
+      <div>
+         My posts
+      <div>
+            new posts
+         <div>
+               post 1
+      </div>
+            <div>
+               post 2
+      </div>
+         </div>
+      </div>
+   </div>
+}
+
+export default Profile;
