108fd4787800063a9da150dbe3428a0589cd61bb
dmitriykosenkov <68489366+dmitriykosenkov@users.noreply.github.com>
2020-11-21 23:08:42 +0200

    Update MyPostsContainer.jsx

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -3,25 +3,6 @@ import MyPosts from './MyPosts';
 import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
 import { connect } from 'react-redux';
 
-// const MyPostsContainer = (props) => {
-//    let state = props.store.getState();
-   
-//    let addPost = () => {
-//       props.store.dispatch(addPostActionCreator());
-//    }
-//    let onPostChange = (text) => {
-//       props.store.dispatch(updateNewPostTextActionCreator(text));
-//    }
-
-//    return (
-//       <MyPosts addPost={addPost} 
-//                updateNewPostText={onPostChange} 
-//                posts={state.profilePage.posts}
-//                newPostText={state.profilePage.newPostText}
-//                />
-//    )
-// }
-
 let mapStateToProps = (state) => {
    return {
       posts: state.profilePage.posts,
@@ -39,8 +20,6 @@ let mapDispatchToProps = (dispatch) => {
       }
    }
 }
-
-
 
 const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts)
 
