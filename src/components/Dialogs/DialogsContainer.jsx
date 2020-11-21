98b67d892b3ae0fd2cd6856e305093fe8a65bd3c
dmitriykosenkov <68489366+dmitriykosenkov@users.noreply.github.com>
2020-11-21 23:09:23 +0200

    Update DialogsContainer.jsx

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -3,26 +3,6 @@ import { connect } from 'react-redux';
 import { addMessageActionCreator, updateNewMessageTextActionCreator } from '../../redux/dialog-reducer'
 import Dialogs from './Dialogs';
 
-
-// const DialogsContainer = (props) => {
-//    let state = props.store.getState().dialogsPage;
-
-//    let addMessage = () => {
-//       props.store.dispatch(addMessageActionCreator());
-//    }
-//    let updateMessageText = (text) => {
-//       let action = updateNewMessageTextActionCreator(text);
-//       props.store.dispatch(action);
-//    }
-
-//    return (
-//       <Dialogs addMessage={addMessage} updateMessageText={updateMessageText}
-//                dialogs={state.dialogs}
-//                messages={state.messages} 
-//       />
-//    )
-// }
-
 let mapStateToProps = (state) => {
    return {
       dialogsPage: state.dialogsPage
